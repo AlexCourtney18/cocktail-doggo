7d7b110f64770284b83e46d7b1220ea82f754f29
samvrny <samvrny@gmail.com>
2022-07-07 14:01:01 -0500

    commiting minor work

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,9 @@ function openPage() {
         window.location.href="./index.html";
         console.log(searchResult);
     }
+    if (resultChopped) { //lines 14-16 added by sam to call his API function from the search history
+        getDogInfo(resultChopped);
+    }
 }
 
 function searchHistory() { //rudimentary way of grabbing the recent search so we can get the information and save it to local storage (not implemented)
@@ -30,13 +33,7 @@ function searchHistory() { //rudimentary way of grabbing the recent search so we
     //If any history is empty, do not show box logic
     //Only show search history when search bar is clicked
     //Also, make it so it saves the history variables to browser
-
     //Would like to make it so clicking the search bar icon does a search, but didn't get to it
-
-
-
-
-
 
 
 
