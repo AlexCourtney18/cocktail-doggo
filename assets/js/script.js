e22e3a094f455d64024b8ebc6f2aa7818861d956
Jonathan Thompson <jathompson399@gmail.com>
2022-07-07 21:20:05 -0500

    haven commit in a while

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
-const dogs = ["Lab", "Golden", "Poodle", "Bully", "Terrier"];
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
 
-const search = document.getElementById("search");
-const matchList = document.getElementById("match-list");
+let users = [];
 
-function searchDogs(searchText) {
-    console.log("fuck");
-}
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase();
+  users.forEach(user => {
+    const isVisible =
+      user.name.toLowerCase().includes(value) ||
+      user.email.toLowerCase().includes(value)
+    user.element.classList.toggle("hide", !isVisible)
+  });
+});
 
-
-search.addEventListener("input", () => searchDogs(search.value));
-
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => res.json())
+  .then(data => {
+    users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      header.textContent = user.name;
+      body.textContent = user.email;
+      userCardContainer.append(card);
+      return { name: user.name, email: user.email, element: card };
+    });
+  });
 
 
 
