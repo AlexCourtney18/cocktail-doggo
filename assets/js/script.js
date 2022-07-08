68f59c04e4be77f4d0abe3e8749270bb7a7eb383
Jonathan Thompson <jathompson399@gmail.com>
2022-07-07 21:47:16 -0500

    implement api

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,10 @@ searchInput.addEventListener("input", e => {
     });
 });
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://dog.ceo/api/breeds/list/all")
   .then(res => res.json())
   .then(data => {
-    users = data.map(user => {
+    users = data.map(data => {
       const card = userCardTemplate.content.cloneNode(true).children[0];
       const header = card.querySelector("[data-header]");
       const body = card.querySelector("[data-body]");
@@ -26,6 +26,40 @@ fetch("https://jsonplaceholder.typicode.com/users")
       return { name: user.name, email: user.email, element: card };
     });
   });
+
+
+
+
+
+// const userCardTemplate = document.querySelector("[data-user-template]");
+// const userCardContainer = document.querySelector("[data-user-cards-container]");
+// const searchInput = document.querySelector("[data-search]");
+
+
+// let users = [] 
+
+// searchInput.addEventListener("input", e => {
+//     const value = e.target.value.toLowerCase();
+//     users.forEach(user => {
+//         const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+//         user.element.classList.toggle("hide", !isVisible);
+//     });
+// });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(res => res.json())
+//   .then(data => {
+//     users = data.map(data => {
+//       const card = userCardTemplate.content.cloneNode(true).children[0];
+//       const header = card.querySelector("[data-header]");
+//       const body = card.querySelector("[data-body]");
+//       header.textContent = user.name;
+//       body.textContent = user.email;
+//       userCardContainer.append(card);
+//       return { name: user.name, email: user.email, element: card };
+//     });
+//   });
+
 
 
 
