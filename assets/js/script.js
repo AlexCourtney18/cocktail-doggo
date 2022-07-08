e983531c7c3175c5f12ce9d558fdde4d6d1c035f
Jonathan Thompson <jathompson399@gmail.com>
2022-07-08 17:54:06 -0500

    im figuring it out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,49 @@
-import { createMenuBreed } from "./util";
-
 const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 const url = "https://dog.ceo/api/breeds/list/all";
 
 
-const dogAPI = async () => {
-  try {
-    let response = await fetch(url);
-    let data = await response.json();
 
-    let breedGroup = Object.values(data.message);
-    let breedName = Object.keys(data.message);
-    return breedGroup.map((subBreed) => {
-      let breedIndex = breedGroup.indexOf(subBreed);
-      let breedName = breedListNames[breedIndex];
-      if(subBreed.length > 0) {
-        return createMenuBreedGroup(breedName, subBreed);
-      }
-      return createMenuBreed(breedName);
-    });
-  }
-}
-dogAPI();
+const dogApi = async () => {
+  let response = await fetch(url);
+  let data = await response.json();
+  let breedGroup = Object.values(data.message);
+  let breedName = Object.keys(data.message);
+  const card = userCardTemplate.content.cloneNode(true).children[0];
+  const header = card.querySelector("[data-header]");
+  const body = card.querySelector("[data-body]");
+  header.textContent = breedGroup
+  body.textContent = breedName
+     console.log(breedGroup);
+     console.log(breedName)
+};
+dogApi();
 
 
+// let users = [];
 
+// searchInput.addEventListener("input", e => {
+//     const value = e.target.value.toLowerCase();
+//     users.forEach(user => {
+//         const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+//         user.element.classList.toggle("hide", !isVisible);
+//     });
+// });
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(res => res.json())
+//   .then(data => {
+//     users = data.map(user => {
+//       const card = userCardTemplate.content.cloneNode(true).children[0];
+//       const header = card.querySelector("[data-header]");
+//       const body = card.querySelector("[data-body]");
+//       header.textContent = user.name;
+//       body.textContent = user.email;
+//       userCardContainer.append(card);
+//       return { name: user.name, email: user.email, element: card };
+//     });
+//   });
 
 
 // const userCardTemplate = document.querySelector("[data-user-template]");
