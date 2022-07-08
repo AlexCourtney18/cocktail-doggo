a323e8989ef6b35b5ad59b18772717cf32082d0a
Jonathan Thompson <jathompson399@gmail.com>
2022-07-07 21:37:27 -0500

    finshed code for search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,15 @@ const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 
-let users = [];
+
+let users = [] 
 
 searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase();
-  users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
-  });
+    const value = e.target.value.toLowerCase();
+    users.forEach(user => {
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+        user.element.classList.toggle("hide", !isVisible);
+    });
 });
 
 fetch("https://jsonplaceholder.typicode.com/users")
