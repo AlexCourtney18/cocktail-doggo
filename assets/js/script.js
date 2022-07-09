22bf132e2e956d3eff3be601daccfae56b191e56
Jonathan Thompson <jathompson399@gmail.com>
2022-07-09 11:14:49 -0500

    push for merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,20 @@ function openPage() {
     getBreed(resultChopped);
 }
 
+
+
+// Reference to Card Template
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+const url = "https://dog.ceo/api/breeds/list/all";
+
+let breeds = [];
+
+const handleSearchInput = (event) => {
+  // Clear Card Container
+  userCardContainer.innerHTML = "";
+
   // Get Search Textbox Value
   const searchTerm = event.target.value.toLowerCase();
   
@@ -33,6 +47,41 @@ function openPage() {
   if (searchTerm === "") {
     return;
   };
+
+  // Filter Breeds by Search Term 
+  const filterBreeds = breeds.filter(breed => {
+    return breed.includes(searchTerm);
+  });
+
+  // Add Cards of Filtered Breeds
+  filterBreeds.forEach(breed => {
+    // Clone Card Template
+    const card = userCardTemplate.content.cloneNode(true).children[0];
+    const body = card.querySelector("[data-body]");
+
+    // Set Cloned Card Text to Breed Name
+    body.textContent = breed;
+
+    // Append Card to Container
+    userCardContainer.append(card);
+  });
+};
+
+// Search Input Event Listener
+searchInput.addEventListener("input", handleSearchInput);
+
+
+const getDogBreeds = async () => {
+  let response = await fetch(url);
+  let data = await response.json();
+  breeds = Object.keys(data.message);
+};
+
+getDogBreeds();
+
+// fetch dog sub-breed list
+function getBreed(resultChopped) {
+    var apiUrl = "https://dog.ceo/api/breed/" + resultChopped + "/list";
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
@@ -49,11 +98,8 @@ function openPage() {
                         }
                         console.log(subBreed + " SUB BREED");
 
-  // Add Cards of Filtered Breeds
-  filterBreeds.forEach(breed => {
-    // Clone Card Template
-    const card = userCardTemplate.content.cloneNode(true).children[0];
-    const body = card.querySelector("[data-body]");
+                        // create a container for each sub-breed
+                        var buttonEl = document.createElement("button");
 
                         // create a span element to hold sub-breed name
                         var titleEl = document.createElement("span");
@@ -329,201 +375,17 @@ function printDoggieFacts() {
 
     //Pseudo Code
 
-
-const getDogBreeds = async () => {
-  let response = await fetch(url);
-  let data = await response.json();
-  breeds = Object.keys(data.message);
-};
-
-getDogBreeds();
+    //Search for something
+    //Search becomes history 1, history 2 becomes history 1, and history 3 becomes history 2
+    //If any history is empty, do not show box logic
+    //Only show search history when search bar is clicked
+    //Also, make it so it saves the history variables to browser
+    //Would like to make it so clicking the search bar icon does a search, but didn't get to it
 
 
 
-// var subBreedButtonEl = document.querySelector("#sub-button");
-// var subImagesEl = document.querySelector("#sub-images");
-// var resultChopped;
-
-// function openPage() {
-//     var searchResult = document.getElementById("search").value; // Grabs result
-//     resultChopped = searchResult.toLowerCase().replace(/\s/g, ''); // Cuts out spaces and makes all lowercase to search easier
-//     console.log(searchResult);
-//     console.log(resultChopped);
-//     if (resultChopped) {
-//         getDogInfo(resultChopped);
-//     }
-//     // if (resultChopped === "bulldog") {
-//     //     window.location.href = "./index.html";
-//     //     console.log(searchResult);
-//     // }
-
-//     // if (resultChopped === "goldenretriever") { //Change this to function as "Check database for resultChopped and if it's there, go there"
-//     //     window.location.href = "./index.html";
-//     //     console.log(searchResult);
-//     // }
-//     // call sub-breed api function
-//     getBreed(resultChopped);
-//     //getBreedImage(resultChopped);
-// }
-
-// // fetch dog sub-breed list
-// function getBreed(resultChopped) {
-//     var apiUrl = "https://dog.ceo/api/breed/" + resultChopped + "/list";
-
-//     fetch(apiUrl).then(function (response) {
-//         if (response.ok) {
-//             response.json().then(function (data) {
-//                 console.log(data);
-//                 createButton(data);
-//                 // create buttons for sub-breeds
-//                 function createButton(data) {
-//                     // clear any existing buttons
-//                     subBreedButtonEl.textContent = "";
-//                     for (var i = 0; i < data.message.length; i++) {
-//                         if (data.message[i]) {
-//                             subBreed = data.message[i];
-//                             //console.log(subBreed + "SUB BREED");
-//                         }
-//                         console.log(subBreed + " SUB BREED");
-
-//                         // create a container for each sub-breed
-//                         var buttonEl = document.createElement("button");
-
-//                         // create a span element to hold sub-breed name
-//                         var titleEl = document.createElement("span");
-//                         titleEl.textContent = subBreed;
-
-//                         buttonEl.appendChild(titleEl);
-
-//                         subBreedButtonEl.appendChild(buttonEl);
-
-//                         // add click event listener for sub-breed buttons
-//                         subBreedButtonEl.addEventListener("click", buttonClick);
-//                         //console.log(click);
-//                     }
-//                 }
-//                 if (data.message.length === 0) {
-//                     console.log("BLANK ARRAY");
-//                     getBreedImage(resultChopped);
-//                     // fetch breed images
-//                     function getBreedImage(resultChopped) {
-//                         var apiImageUrl = "https://dog.ceo/api/breed/" + resultChopped + "/images";
-
-//                         fetch(apiImageUrl).then(function (response) {
-//                             if (response.ok) {
-//                                 response.json().then(function (data) {
-//                                     console.log(data);
-
-//                                     for (var i = 0; i < 4; i++) {
-//                                         if (data.message[i]) {
-
-//                                             var subParentEl = document.createElement("div");
-//                                             subImagesEl.appendChild(subParentEl);
-
-//                                             subImage = data.message[i];
-
-//                                             console.log(subImage + " SUB IMAGE");
-
-//                                             // create a container for each sub-image
-//                                             var imageEl = document.createElement("img");
-//                                             imageEl.setAttribute("src", subImage);
-//                                             subParentEl.appendChild(imageEl);
-//                                         }
-//                                     }
-//                                 });
-//                             } else {
-//                                 console.log("breed IMAGE not found");
-//                             }
-//                         });
-//                     }
-//                 }
-//             });
-//         } else {
-//             console.log("dog breed not found");
-//         }
-//     });
-// }
-
-// function buttonClick(event) {
-//     var btnClick = event.target.textContent;
-//     getBreedImage(resultChopped + "/" + btnClick);
-// }
-
-// // fetch breed image
-// function getBreedImage(resultChopped) {
-//     var apiImageUrl = "https://dog.ceo/api/breed/" + resultChopped + "/images";
-
-//     subImagesEl.textContent = "";
-//     fetch(apiImageUrl).then(function (response) {
-//         if (response.ok) {
-//             response.json().then(function (data) {
-//                 console.log(data);
-
-//                 for (var i = 0; i < 4; i++) {
-//                     if (data.message[i]) {
-
-//                         var subParentEl = document.createElement("div");
-//                         subImagesEl.appendChild(subParentEl);
-
-//                         subImage = data.message[i];
-
-//                         console.log(subImage + " SUB IMAGE");
-
-//                         // create a container for each sub-image
-//                         var imageEl = document.createElement("img");
-//                         imageEl.setAttribute("src", subImage);
-//                         subParentEl.appendChild(imageEl);
-//                     }
-//                 }
-//             });
-//         } else {
-//             console.log("breed IMAGE not found");
-//         }
-//     });
-// }
-
-// function searchHistory() { //rudimentary way of grabbing the recent search so we can get the information and save it to local storage (not implemented)
-//     var recentSearch = [];
-//     recentSearch.push($('#search').val());
-
-//     $.each(recentSearch, function (index, value) {
-//         const p = document.createElement("p");
-//         p.innerHTML = value;
-//         document.getElementById("historyLine1").appendChild(p);
-//     })
-// }
-
-// //This is the wikipedia API call
-// var getDogInfo = function(resultChopped) {
-     
-//     var dogInfo = "https://en.wikipedia.org/w/api.php?action=query&titles=" + resultChopped + "&prop=extracts&format=json&exintro=1&origin=*" 
-
-//     fetch(dogInfo).then(function(response) {
-//         if (response.ok) {
-//             response.json().then(function(data) {
-//                 console.log(data);
-//             }) 
-//         }
-//     })
-
-// };
-
-//     // JONNY Pseudo Code
-//     //Search for something
-//     //Search becomes history 1, history 2 becomes history 1, and history 3 becomes history 2
-//     //If any history is empty, do not show box logic
-//     //Only show search history when search bar is clicked
-//     //Also, make it so it saves the history variables to browser
-//     //Would like to make it so clicking the search bar icon does a search, but didn't get to it
-    
-
-//     // ALEX Pseudo Code
-//     // when breed family is searched, display clickable buttons of sub-breeds
-//     // if no sub-breeds return (array length 0), but family returns (ex pitbull) - display images of family
-//     // when sub-breed buttons are clicked, display images for sub-breed
-//     // test comments 
-
-
-
-
-
+    // ALEX Pseudo Code
+    // when breed family is searched, display clickable buttons of sub-breeds
+    // if no sub-breeds return (array length 0), but family returns (ex pitbull) - display images of family
+    // when sub-breed buttons are clicked, display images for sub-breed
+    // test comments 
