cceb7e19e3de44baaa766e058aa3c0164766a7e0
Kit Sterling <99767427+kitsterling@users.noreply.github.com>
2022-07-09 21:42:27 -0500

    Feature/kit (#38)
    * First commit to my feature branch
    
    * committing changes that will be overwritten anyway
    
    * outlined some sections of HTML
    
    * worked on search history/search bar, unfinished
    
    * added bulma/font awesome/jquery
    
    * still figuring out search bar stuff
    
    * updated js, cleaned it up after going off on a tangent
    
    * made it so error box shows up and hides based on if a search was successful
    
    * updated html/css and some js. layout modified, search expanded on
    
    * fixed images
    
    * Fonts added to HTML and CSS
    
    * updated webpage shift upon search
    
    * updated search bar and results visually and modified functions
    
    * cleaned up some bugs and made everything neater
    
    * final touch for now
    
    Co-authored-by: AlexCourtney18 <102307529+AlexCourtney18@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,15 @@ var successfulSearchFlag; // This variable is asking "Have you succeeded at a se
 var doggieButtonClick;
 
 searchButtonOriginal.addEventListener("click", openPage);
+searchButtonOriginal.addEventListener("click", clearSearch);
+
+var searchFlag = false; // This variable is asking "Have you searched before?" 
+var successfulSearchFlag; // This variable is asking "Have you succeeded at a search before?"
+
+function clearSearch() {
+    document.querySelector("#dogQ").classList.add('hidden');
+    document.getElementById('search').value = "";
+}
 
 var searchFlag = false; // This variable is asking "Have you searched before?" 
 var successfulSearchFlag; // This variable is asking "Have you succeeded at a search before?"
@@ -24,6 +33,8 @@ function openPage() {
     document.querySelector("#webpage-title").classList.add('titleLefted');
     document.querySelector("#webpage-subtitle").classList.add('subtitleLefted')
     document.querySelector("#search-container").classList.add('searchRighted')
+    document.querySelector("#deckbox").classList.add('resultsRighted')
+
     var searchResult = document.getElementById("search").value; // Grabs result
     resultChopped = searchResult.toLowerCase().replace(/\s/g, ''); // Cuts out spaces and makes all lowercase to search easier
     searchHistory(resultChopped);
@@ -53,8 +64,11 @@ const handleSearchInput = (event) => {
   
   // Don't Add Cards if Search Input is Empty
   if (searchTerm === "") {
+    document.querySelector("#dogQ").classList.add('hidden')
     return;
   };
+
+  document.querySelector("#dogQ").classList.remove('hidden')
 
   // Filter Breeds by Search Term 
   const filterBreeds = breeds.filter(breed => {
@@ -72,13 +86,14 @@ const handleSearchInput = (event) => {
 
     // Append Card to Container
     userCardContainer.append(card);
-
     $(card).on("click", function() {
         resultChopped = body.textContent;
         console.log(resultChopped);
         document.querySelector("#webpage-title").classList.add('titleLefted');
         document.querySelector("#webpage-subtitle").classList.add('subtitleLefted')
         document.querySelector("#search-container").classList.add('searchRighted')
+        document.querySelector("#deckbox").classList.add('resultsRighted')
+
         getDogInfo();
         searchHistory(resultChopped);
         getBreed(resultChopped);
@@ -149,6 +164,8 @@ function getBreed(resultChopped) {
                             if (response.ok) {
                                 response.json().then(function (data) {
                                     console.log(data);
+                                    clearSearch();
+                                    document.querySelector("#main-container").classList.add('vh20');
                                     for (var i = 0; i < 3; i++) {
                                         if (data.message[i]) {
 
@@ -202,6 +219,7 @@ function getBreed(resultChopped) {
             document.querySelector("#error-page-box").classList.remove('hidden');
             document.querySelector("#error-page-content").classList.remove('hidden');
             document.querySelector("#dog-facts").classList.remove('hidden');
+            document.querySelector("#main-container").classList.add('vh25');
             while(statistics.firstChild) {
                 statistics.removeChild(statistics.firstChild);
             }
@@ -270,7 +288,7 @@ function searchHistory() { //rudimentary way of grabbing the recent search so we
     $.each(recentSearch, function (index, value) {
         const p = document.createElement("p");
         p.innerHTML = value;
-        document.getElementById("historyLine1").appendChild(p);
+        console.log(p);
     })
 }
 
@@ -416,20 +434,3 @@ function printDoggieFacts() {
 
     window.scrollTo(0, document.body.scrollHeight);
 };
-
-    //Pseudo Code
-
-    //Search for something
-    //Search becomes history 1, history 2 becomes history 1, and history 3 becomes history 2
-    //If any history is empty, do not show box logic
-    //Only show search history when search bar is clicked
-    //Also, make it so it saves the history variables to browser
-    //Would like to make it so clicking the search bar icon does a search, but didn't get to it
-
-
-
-    // ALEX Pseudo Code
-    // when breed family is searched, display clickable buttons of sub-breeds
-    // if no sub-breeds return (array length 0), but family returns (ex pitbull) - display images of family
-    // when sub-breed buttons are clicked, display images for sub-breed
-    // test comments 
